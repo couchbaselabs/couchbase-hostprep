0ba9cbcacb570834486e522aa752b09eeb429d88
Michael Minichino <mminichino@mac.com>
2022-02-08 13:34:42 -0600

    Updates and Bug Fixes

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -3,5 +3,10 @@
 SCRIPTDIR=$(cd $(dirname $0) && pwd)
 PKGDIR=$(dirname $SCRIPTDIR)
 
+curl -Is --connect-timeout 4 --retry 3 https://github.com >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Can not connect to GitHub. Refresh cancelled."
+fi
+
 cd $PKGDIR
-git pull
+git pull -q
