a2cbbc8101561cac1d0b4f3f905a20010cf60cd5
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-04-25 14:44:12 -0500

    Version 2.0

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -11,12 +11,21 @@ err_exit() {
 source /etc/os-release
 echo "Bootstrap linux type $ID"
 case ${ID:-null} in
-centos|rhel)
+centos|rhel|amzn|rocky|ol|fedora)
   yum install -q -y git
   ;;
-ubuntu)
+ubuntu|debian)
   apt-get update
   apt-get install -q -y git
+  ;;
+opensuse-leap|sles)
+  zypper install -y git
+  ;;
+arch)
+  pacman-key --init
+  pacman-key --populate
+  pacman -Sy --noconfirm
+  pacman -S --noconfirm git
   ;;
 *)
   err_exit "Unknown Linux distribution $ID"
