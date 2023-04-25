5b77f44f7397524eda2ea49c94a1a56cbac11a1b
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-04-25 15:41:29 -0500

    Version 2.0

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -8,28 +8,43 @@ err_exit() {
    exit 1
 }
 
-source /etc/os-release
-echo "Bootstrap linux type $ID"
-case ${ID:-null} in
-centos|rhel|amzn|rocky|ol|fedora)
-  yum install -q -y git
-  ;;
-ubuntu|debian)
-  apt-get update
-  apt-get install -q -y git
-  ;;
-opensuse-leap|sles)
-  zypper install -y git
-  ;;
-arch)
-  pacman-key --init
-  pacman-key --populate
-  pacman -Sy --noconfirm
-  pacman -S --noconfirm git
-  ;;
-*)
-  err_exit "Unknown Linux distribution $ID"
-  ;;
-esac
+do_bootstrap() {
+  source /etc/os-release
+  echo "Bootstrap linux type $ID"
+  case ${ID:-null} in
+  centos|rhel|amzn|rocky|ol|fedora)
+    yum install -q -y git
+    ;;
+  ubuntu|debian)
+    apt-get update
+    apt-get install -q -y git
+    ;;
+  opensuse-leap|sles)
+    zypper install -y git
+    ;;
+  arch)
+    pacman-key --init
+    pacman-key --populate
+    pacman -Sy --noconfirm
+    pacman -S --noconfirm git
+    ;;
+  *)
+    err_exit "Unknown Linux distribution $ID"
+    ;;
+  esac
+}
 
-####
+for i in {1..5}
+do
+  echo "Bootstrap attempt $i on $(date)" >> /var/tmp/hostprep_bootstrap.log
+  if do_bootstrap
+  then
+    echo "Bootstrap successful."
+    break
+  else
+    echo "Unsuccessful on $(date)" >> /var/tmp/hostprep_bootstrap.log
+    sleep 15
+  fi
+done
+
+## end
