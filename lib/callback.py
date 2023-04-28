102541e2927b291531191dca135625391c82a3f9
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-04-28 08:41:07 -0500

    Version 2.0

---
+++ lib/callback.py
@@ -0,0 +1,26 @@
+##
+
+import json
+from ansible.plugins.callback import CallbackBase
+
+
+class ResultsCollectorJSONCallback(CallbackBase):
+
+    def __init__(self, *args, **kwargs):
+        super(ResultsCollectorJSONCallback, self).__init__(*args, **kwargs)
+        self.host_ok = {}
+        self.host_unreachable = {}
+        self.host_failed = {}
+
+    def v2_runner_on_unreachable(self, result):
+        host = result._host
+        self.host_unreachable[host.get_name()] = result
+
+    def v2_runner_on_ok(self, result, *args, **kwargs):
+        host = result._host
+        self.host_ok[host.get_name()] = result
+        print(json.dumps({host.name: result._result}, indent=4))
+
+    def v2_runner_on_failed(self, result, *args, **kwargs):
+        host = result._host
+        self.host_failed[host.get_name()] = result
