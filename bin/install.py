04682b77ac2d85d0456f03d54b27831e34936112
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-08-03 13:52:35 -0500

    Version 2.0

--- bin/install.py
+++ bin/install.py
@@ -5,10 +5,13 @@ import warnings
 import argparse
 import sys
 import os
+import io
 import signal
 import inspect
 import traceback
+import datetime
 import ansible_runner
+from datetime import datetime
 from hostpreplib.bundles import SoftwareBundle
 
 warnings.filterwarnings("ignore")
@@ -41,17 +44,23 @@ class Parameters(object):
 
 
 class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    green = "\x1b[32;20m"
+    reset = "\x1b[0m"
     format_level = "%(levelname)s"
     format_name = "%(name)s"
     format_message = "%(message)s"
     format_line = "(%(filename)s:%(lineno)d)"
     format_extra = " [%(name)s](%(filename)s:%(lineno)d)"
     FORMATS = {
-        logging.DEBUG: f"{format_level} - {format_message}",
-        logging.INFO: f"{format_level} - {format_message}",
-        logging.WARNING: f"{format_level} - {format_message}",
-        logging.ERROR: f"{format_level} - {format_message}",
-        logging.CRITICAL: f"{format_level} - {format_message}"
+        logging.DEBUG: f"{grey}{format_level}{reset} - {format_message}",
+        logging.INFO: f"{green}{format_level}{reset} - {format_message}",
+        logging.WARNING: f"{yellow}{format_level}{reset} - {format_message}",
+        logging.ERROR: f"{red}{format_level}{reset} - {format_message}",
+        logging.CRITICAL: f"{red}{format_level}{reset} - {format_message}"
     }
 
     def format(self, record):
@@ -60,6 +69,20 @@ class CustomFormatter(logging.Formatter):
             log_fmt += self.format_extra
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
+
+
+class StreamToLogger(object):
+    def __init__(self, _logger, _level):
+        self.logger = _logger
+        self.level = _level
+        self.buffer = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.level, line.rstrip())
+
+    def flush(self):
+        pass
 
 
 class RunMain(object):
@@ -71,55 +94,63 @@ class RunMain(object):
         self.op = SoftwareBundle(f"{self.parent}/config/packages.json")
 
     def run(self, options):
+        os_name = self.op.os.os_name
+        os_major = self.op.os.os_major_release
+        os_minor = self.op.os.os_minor_release
+        logger.info(f"Running on {os_name} release {os_major}")
         extra_vars = {
-            'os_name': self.op.os.os_name,
-            'os_major': self.op.os.os_major_release,
-            'os_minor': self.op.os.os_minor_release,
+            'os_name': os_name,
+            'os_major': os_major,
+            'os_minor': os_minor,
         }
+
         for b in options.bundles:
             self.op.add(b)
+
+        timestamp = datetime.utcnow().strftime("%b %d %H:%M:%S")
+        logger.info(f" ==== Run begins {timestamp} ====")
+
         for bundle in self.op.install_list():
+            logger.info(f"Executing bundle {bundle.name}")
             for playbook in [bundle.pre, bundle.run, bundle.post]:
+                f = io.StringIO()
                 if not playbook:
                     continue
-                r = ansible_runner.run(playbook=f"{self.parent}/playbooks/{playbook}", extravars=extra_vars)
-                print(f"{r.status}: {r.rc}")
-                for each_host_event in r.events:
-                    print(each_host_event['event'])
-                print(f"Final status: {r.stats}")
+                logger.info(f"Running playbook {playbook}")
+                stdout_save = sys.stdout
+                sys.stdout = StreamToLogger(logger, logging.INFO)
+                r = ansible_runner.run(playbook=f"{self.parent}/playbooks/{playbook}", extravars=extra_vars, _output=f)
+                sys.stdout = stdout_save
+                logger.info(f"Playbook status: {r.status}")
+                if r.rc != 0:
+                    logger.error(r.stats)
+                    timestamp = datetime.utcnow().strftime("%b %d %H:%M:%S")
+                    logger.info(f" ==== Run failed {timestamp} ====")
+                    sys.exit(r.rc)
+
+        timestamp = datetime.utcnow().strftime("%b %d %H:%M:%S")
+        logger.info(f" ==== Run finished {timestamp} ====")
 
 
 def main():
     global logger
     signal.signal(signal.SIGINT, break_signal_handler)
-    default_debug_file = f"{os.environ['HOME']}/{os.path.splitext(os.path.basename(__file__))[0]}_debug.out"
+    default_debug_file = f"/var/log/hostprep.log"
     debug_file = os.environ.get("DEBUG_FILE", default_debug_file)
     arg_parser = Parameters()
     parameters = arg_parser.parameters
 
-    try:
-        if parameters.debug:
-            logger.setLevel(logging.DEBUG)
+    if parameters.debug:
+        logger.setLevel(logging.DEBUG)
+        screen_handler = logging.StreamHandler()
+        screen_handler.setFormatter(CustomFormatter())
+        logger.addHandler(screen_handler)
+    else:
+        logger.setLevel(logging.INFO)
 
-            try:
-                open(debug_file, 'w').close()
-            except Exception as err:
-                print(f"[!] Warning: can not clear log file {debug_file}: {err}")
-
-            file_handler = logging.FileHandler(debug_file)
-            file_formatter = logging.Formatter(logging.BASIC_FORMAT)
-            file_handler.setFormatter(file_formatter)
-            logger.addHandler(file_handler)
-        elif parameters.verbose:
-            logger.setLevel(logging.INFO)
-        else:
-            logger.setLevel(logging.ERROR)
-    except (ValueError, KeyError):
-        pass
-
-    screen_handler = logging.StreamHandler()
-    screen_handler.setFormatter(CustomFormatter())
-    logger.addHandler(screen_handler)
+    file_handler = logging.FileHandler(debug_file)
+    file_handler.setFormatter(CustomFormatter())
+    logger.addHandler(file_handler)
 
     RunMain().run(parameters)
 
