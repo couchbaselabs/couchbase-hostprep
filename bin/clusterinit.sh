803f55cdf86d75c6c1b9ed7559eb2f985b938949
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-08-10 22:19:47 -0500

    Added data disk automation

--- bin/clusterinit.sh
+++ bin/clusterinit.sh
@@ -14,6 +14,7 @@ USERNAME="Administrator"
 PASSWORD="password"
 CLUSTER_NAME="cbdb"
 if [ -d /cbdata ]; then
+  chown couchbase:couchbase /cbdata
   DATAPATH=/cbdata
   INDEXPATH=/cbdata
   ANALYTICSPATH=/cbdata
